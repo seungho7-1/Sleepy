0bf6a66e8b17f12b2c815b94967e27f7410cab05
Seung ho <kshdeco@naver.com>
2026-08-08 00:11:08 +0900

    Add category normalization script

---
+++ src/main/resources/db/migration/V10__normalize_product_categories.sql
@@ -0,0 +1,5 @@
+﻿UPDATE product SET category = 'SLIME' WHERE category = '슬라임';
+UPDATE product SET category = 'SLANGY' WHERE category IN ('슬랑이', 'SLANGI');
+UPDATE product SET category = 'MALLANGI' WHERE category IN ('말랑이', 'MALANGI');
+UPDATE product SET category = 'SQUISHY' WHERE category = '스퀴시';
+UPDATE product SET category = 'WAKPPU' WHERE category = '왁뿌';
